625f3a71c872702b1614a6947410b1109bf82793
QHuyyyyy <huy120904@gmail.com>
2025-11-07 00:24:12 +0700

    register

--- app/src/main/java/com/example/prm_assignment/data/remote/AuthApi.kt
+++ app/src/main/java/com/example/prm_assignment/data/remote/AuthApi.kt
@@ -3,6 +3,8 @@ package com.example.prm_assignment.data.remote
 import com.example.prm_assignment.data.model.LoginRequest
 import com.example.prm_assignment.data.model.LoginResponse
 import com.example.prm_assignment.data.model.ProfileResponse
+import com.example.prm_assignment.data.model.RegisterRequest
+import com.example.prm_assignment.data.model.VerifyOtpRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.GET
@@ -11,6 +13,12 @@ import retrofit2.http.Header
 interface AuthApi {
     @POST("auth/login-by-password")
     suspend fun login(@Body request: LoginRequest): LoginResponse
+
+    @POST("auth/register-customer")
+    suspend fun register(@Body request: RegisterRequest): LoginResponse
+
+    @POST("auth/register-customer/verify")
+    suspend fun verifyRegister(@Body request: VerifyOtpRequest): LoginResponse
 
     @GET("auth/profile")
     suspend fun getProfile(@Header("Authorization") token: String): ProfileResponse
